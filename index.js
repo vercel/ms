9992f8a962332c4071ce9c6f0750358b1ddbf36b
Ekin Koc <ekin@eknkc.com>
2013-03-07 23:04:19 +0200

    Fixes #22 where minutes does not work due to an unfortunately named variable.

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ module.exports = function(val, options){
  */
 
 function parse(str) {
-  var m = /^((?:\d+)?\.?\d+) *(ms|seconds?|s|minutes?|m|hours?|h|days?|d|years?|y)?$/i.exec(str);
-  if (!m) return;
-  var n = parseFloat(m[1]);
-  var type = (m[2] || 'ms').toLowerCase();
+  var match = /^((?:\d+)?\.?\d+) *(ms|seconds?|s|minutes?|m|hours?|h|days?|d|years?|y)?$/i.exec(str);
+  if (!match) return;
+  var n = parseFloat(match[1]);
+  var type = (match[2] || 'ms').toLowerCase();
   switch (type) {
     case 'years':
     case 'year':
