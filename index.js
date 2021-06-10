716b4cf0618a4c76dc719ed62ff45aa1f9e3f492
culinaryAmphibian <teperbudget@gmail.com>
2021-06-09 20:28:03 -0400

    add style and more cases
    make if statements one line if applicable, remove if-else, add more cases to the case switch, format the case switch

--- index.js
+++ index.js
@@ -26,11 +26,8 @@ var y = d * 365.25;
 module.exports = function (val, options) {
   options = options || {};
   var type = typeof val;
-  if (type === 'string' && val.length > 0) {
-    return parse(val);
-  } else if (type === 'number' && isFinite(val)) {
-    return options.long ? fmtLong(val) : fmtShort(val);
-  }
+  if (type === 'string' && val.length > 0) return parse(val);
+  if (type === 'number' && isFinite(val)) return options.long ? fmtLong(val) : fmtShort(val);
   throw new Error(
     'val is not a non-empty string or a valid number. val=' +
       JSON.stringify(val)
@@ -47,55 +44,27 @@ module.exports = function (val, options) {
 
 function parse(str) {
   str = String(str);
-  if (str.length > 100) {
-    return;
-  }
+  if (str.length > 100) return;
   var match = /^(-?(?:\d+)?\.?\d+) *(milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|weeks?|w|years?|yrs?|y)?$/i.exec(
     str
   );
-  if (!match) {
-    return;
-  }
+  if (!match) return;
   var n = parseFloat(match[1]);
   var type = (match[2] || 'ms').toLowerCase();
   switch (type) {
-    case 'years':
-    case 'year':
-    case 'yrs':
-    case 'yr':
-    case 'y':
+    case 'years', 'year', 'yrs', 'yr', 'y':
       return n * y;
-    case 'weeks':
-    case 'week':
-    case 'w':
+    case 'weeks', 'week', 'wk', 'w':
       return n * w;
-    case 'days':
-    case 'day':
-    case 'd':
+    case 'days', 'day', 'dy', 'd':
       return n * d;
-    case 'hours':
-    case 'hour':
-    case 'hrs':
-    case 'hr':
-    case 'h':
+    case 'hours', 'hour', 'hrs', 'hr', 'h':
       return n * h;
-    case 'minutes':
-    case 'minute':
-    case 'mins':
-    case 'min':
-    case 'm':
+    case 'minutes', 'minute', 'mins', 'min', 'm':
       return n * m;
-    case 'seconds':
-    case 'second':
-    case 'secs':
-    case 'sec':
-    case 's':
+    case 'seconds', 'second', 'secs', 'sec', 's':
       return n * s;
-    case 'milliseconds':
-    case 'millisecond':
-    case 'msecs':
-    case 'msec':
-    case 'ms':
+    case 'milliseconds', 'millisecond', 'msecs', 'msec', 'ms':
       return n;
     default:
       return undefined;
@@ -112,18 +81,10 @@ function parse(str) {
 
 function fmtShort(ms) {
   var msAbs = Math.abs(ms);
-  if (msAbs >= d) {
-    return Math.round(ms / d) + 'd';
-  }
-  if (msAbs >= h) {
-    return Math.round(ms / h) + 'h';
-  }
-  if (msAbs >= m) {
-    return Math.round(ms / m) + 'm';
-  }
-  if (msAbs >= s) {
-    return Math.round(ms / s) + 's';
-  }
+  if (msAbs >= d) return Math.round(ms / d) + 'd';
+  if (msAbs >= h) return Math.round(ms / h) + 'h';
+  if (msAbs >= m) return Math.round(ms / m) + 'm';
+  if (msAbs >= s) return Math.round(ms / s) + 's';
   return ms + 'ms';
 }
 
@@ -137,18 +98,10 @@ function fmtShort(ms) {
 
 function fmtLong(ms) {
   var msAbs = Math.abs(ms);
-  if (msAbs >= d) {
-    return plural(ms, msAbs, d, 'day');
-  }
-  if (msAbs >= h) {
-    return plural(ms, msAbs, h, 'hour');
-  }
-  if (msAbs >= m) {
-    return plural(ms, msAbs, m, 'minute');
-  }
-  if (msAbs >= s) {
-    return plural(ms, msAbs, s, 'second');
-  }
+  if (msAbs >= d) return plural(ms, msAbs, d, 'day');
+  if (msAbs >= h) return plural(ms, msAbs, h, 'hour');
+  if (msAbs >= m) return plural(ms, msAbs, m, 'minute');
+  if (msAbs >= s) return plural(ms, msAbs, s, 'second');
   return ms + ' ms';
 }
 
