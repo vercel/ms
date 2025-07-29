e5f956f6593a1b34e1799429cc22016cee314ba6
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2025-07-29 16:43:22 -0400

    updates typescript, build, lint, format

---
+++ tsdown.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  minify: true,
+});
