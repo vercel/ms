0b5423c66997fda31c054de99837bb38a11a693b
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2025-08-01 09:06:37 -0400

    formatting

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,12 @@ const w = d * 7;
 const y = d * 365.25;
 
 type Years = 'years' | 'year' | 'yrs' | 'yr' | 'y';
-
 type Weeks = 'weeks' | 'week' | 'w';
-
 type Days = 'days' | 'day' | 'd';
-
 type Hours = 'hours' | 'hour' | 'hrs' | 'hr' | 'h';
-
 type Minutes = 'minutes' | 'minute' | 'mins' | 'min' | 'm';
-
 type Seconds = 'seconds' | 'second' | 'secs' | 'sec' | 's';
-
 type Milliseconds = 'milliseconds' | 'millisecond' | 'msecs' | 'msec' | 'ms';
-
 type Unit = Years | Weeks | Days | Hours | Minutes | Seconds | Milliseconds;
 
 type UnitAnyCase = Capitalize<Unit> | Uppercase<Unit> | Unit;
